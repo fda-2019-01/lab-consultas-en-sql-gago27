31db9e5506fd62a5f9331e0e6ed80e4a9e4a3ecb
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-28 11:00:54 -0500

    Update q12.sql

--- q12.sql
+++ q12.sql
@@ -3,4 +3,33 @@
 ## de registros de la tabla tbl1 para el ano
 ## 2018
 ##
+import sqlite3, os
+import glob 
+import sys
 
+filenames = glob.glob("tbl1.csv")
+conn = sqlite3.connect(":memory:")
+cur = conn.cursor()
+
+tabla ="""
+CREATE TABLE tbl1(
+  K0 CHAR(1),
+  K1 INT,
+  c12 FLOAT,
+  c13 INT,
+  c14 DATE,
+  c15 FLOAT,
+  c16 CHAR(4)
+  );
+"""
+cur.execute(tabla).fetchall()
+
+tabla1 = open("tbl1.csv", "rt", encoding="utf-8").readlines()
+tabla0 = [line[:-1] for line in tabla1[:-1]]
+tabla0.append(tabla1[-1])
+tabla1 = tabla0
+tabla1 = [line.split(",") for line in tabla1]
+tabla1 = [tuple(line) for line in tabla1]
+
+cur.executemany("INSERT INTO tbl1 VALUES(?,?,?,?,?,?,?)", tabla1).fetchall()
+cur.execute("SELECT COUNT(*) FROM tbl1 WHERE c14 > '2017-12-31' AND(c14 < '2019-01-01')").fetchall()
