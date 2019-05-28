2de2891d0e89a34a8e30a858cca9a03b6f9ad97e
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-28 10:55:25 -0500

    Update q06.sql

--- q06.sql
+++ q06.sql
@@ -3,5 +3,30 @@
 ## la tabla tbl1 ordenada por fecha (c14) para los 
 ## registros con K0 igual a A
 ## 
+import sqlite3
+import glob
 
+filenames= glob.glob("tbl1.csv")
+conn = sqlite3.connect(":memory:")
+cur = conn.cursor()
+tabla = """
+CREATE TABLE tbl1 ( 
+  K0 CHAR(1),
+  K1 INT,
+  c12 FLOAT,
+  c13 INT,
+  c14 DATE,
+  c15 FLOAT,
+  c16 CHAR(4)
+  );
+"""
+conn.execute(tabla).fetchall()
+tablas = open("tbl1.csv", "rt", encoding="utf-8").readlines()
+tablas1 = [line[:-1] for line in tablas[:-1]]
+tablas1.append(tablas[-1])
+tablas1 = [line.split(",") for line in tablas1]
+tablas = tablas1
+tablas = [tuple(line) for line in tablas]
+conn.executemany("INSERT INTO tbl1 VALUES(?,?,?,?,?,?,?)", tablas).fetchall()
+conn.execute("SELECT * FROM tbl1 WHERE K0 = 'A' ORDER BY c14").fetchall()
 
