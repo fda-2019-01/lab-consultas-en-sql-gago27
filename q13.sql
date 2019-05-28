31efa0aa0bbda774481b7c9f6fbe180c6b505a61
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-28 11:08:57 -0500

    Update q13.sql

--- q13.sql
+++ q13.sql
@@ -3,5 +3,33 @@
 ## de la columna K0, el valor máximo de la coumna c12
 ## en la tabla tbl1.
 ##
+import sqlite3
+import glob
 
+filenames = glob.glob("tbl1.csv")
+conn = sqlite3.connect(":memory:")
+cur= conn.cursor()
+
+tabla = """
+  CREATE TABLE tbl1(
+  K0 CHAR(1),
+  K1 INT,
+  c12 FLOAT,
+  c13 INT,
+  c14 DATE,
+  c15 FLOAT,
+  c16 CHAR(4)
+  );
+"""
+cur.execute(tabla).fetchall()
+
+tabla2 = open('tbl1.csv', 'rt', encoding='utf-8').readlines()
+tabla1 = [line[:-1] for line in tabla2[:-1]]
+tabla1.append(tabla2[-1])
+tabla2= tabla1
+tabla2 = [line.split(",") for line in tabla2]
+tabla2 = [tuple(line) for line in tabla2]
+
+cur.executemany("INSERT INTO tbl1 VALUES(?,?,?,?,?,?,?)", tabla2).fetchall()
+cur.execute("SELECT K0, MAX(c12) FROM tbl1 GROUP BY K0").fetchall()
 
